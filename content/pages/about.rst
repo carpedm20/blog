ace5c3922bb44817f8b9f2da70804f6058931bde
carpedm20 <carpedm20@gmail.com>
2014-06-29 03:15:12 +0900

    first commit

---
+++ content/pages/about.rst
@@ -0,0 +1,56 @@
+:slug: about
+:comments: no
+
+.. image:: /images/me2.jpg
+    :alt: Portrait picture of me
+    :align: right
+    :width: 144
+    :height: 226
+
+Profile
+-------
+
+- Python_ programmer
+- Two idiots
+- Blog :: `carpedm20.com`_
+- Github :: `github.com/carpedm20`_
+- Linkedin :: `linkedin.com/in/carpedm20`_
+
+Awards
+------
+
+- `ISC14`_ Student Cluster Challenge, Finalist
+- `Codegate 2014`_ quals, 17th place
+- Hacking festival from The Catholic University of Korea, 1st place
+- Korea Whitehat Contest Hacking festival, 3rd place
+- 2013 Asia Student Supercomputing Challenge, Finalist
+
+Projects
+--------
+
+2014
+~~~~
+- MovieTag
+- 123
+
+2013
+~~~~
+- MovieTag
+- 123
+
+Presentation
+------------
+
+- `쉽게 쓰여진 Django`_ (14-03-19)
+- `영화 서비스에 대한 생각`_ (14-03-05)
+
+.. _carpedm20.com: http://carpedm20.com
+.. _github.com/carpedm20: https://github.com/carpedm20
+.. _linkedin.com/in/carpedm20: https://www.linkedin.com/in/carpedm20
+.. _isc14: http://www.isc-events.com/isc14/student-cluster-competition.html
+.. _codegate 2014: http://codegate.org/
+.. _쉽게 쓰여진 Django: http://www.slideshare.net/carpedm20/django-32473577
+.. _영화 서비스에 대한 생각: http://www.slideshare.net/carpedm20/ss-32447808
+.. _python: http://python.org/
+.. _django: https://www.djangoproject.org
+.. _github: https://github.com/
---
+++ pelicanconf.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'carpedm20'
+SITENAME = u'carpedm20'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+DEFAULT_LANG = u'ko'
+
+# Metadata
+USE_FOLDER_AS_CATEGORY = False
+DEFAULT_CATEGORY = 'Blog'
+DEFAULT_DATE_FORMAT = '%B %d, %Y'
+FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})_(?P<slug>.*)'
+
+# Markup
+MARKUP = ('rst',)
+
+# Feed generation is usually not desired when developing
+FEED_RSS = 'feeds/rss.xml'
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Build process
+STATIC_PATHS = ['images', 'files']
+TYPOGRIFY = False  # TODO blocked by pelican issue #980
+SUMMARY_MAX_LENGTH = 15
+DEFAULT_PAGINATION = 10
+DEFAULT_ORPHANS = 2
+
+# URL configuration
+RELATIVE_URLS = True  # dev only
+ARTICLE_URL = '{date:%Y}/{date:%-m}/{date:%-d}/{slug}/'
+ARTICLE_SAVE_AS = ARTICLE_URL + 'index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = PAGE_URL + 'index.html'
+TAG_URL = 'tags/{slug}/'
+TAG_SAVE_AS = TAG_URL + 'index.html'
+TAGS_URL = 'tags/'
+TAGS_SAVE_AS = 'tags/index.html'
+ARCHIVES_SAVE_AS = 'archive/index.html'
+ARCHIVES_URL = 'archive/'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%-m}/index.html'
+DAY_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%-m}/{date:%-d}/index.html'
+PAGE_LANG_SAVE_AS = False
+ARTICLE_LANG_SAVE_AS = False
+AUTHOR_SAVE_AS = False
+AUTHORS_SAVE_AS = False
+CATEGORY_SAVE_AS = False
+
+# Blogroll
+LINKS =  (('Name', 'http://url.com/'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'http://twitter.com/username'),
+          ('LinkedIn', 'http://twitter.com/username'),
+          ('Github', 'http://twitter.com/username'),)
+
+DEFAULT_PAGINATION = 10
+
+# Template
+THEME = "themes/dbrgn"
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
