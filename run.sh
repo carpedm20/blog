f9658d7fda6e49c5029569fc85244426b5bc338e
Taehoon Kim <carpedm20@gmail.com>
2014-08-24 09:59:46 +0900

    blog update

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@
 #cp -r ../old_carpedm20.github.com/ndrive ./output/
 #cp -r ../old_carpedm20.github.com/randomwall ./output/
 pelican content; git add .; git commit -m "blog update"; git push origin master;
-echo "cp -r ./output/* ../carpedm20.github.io/"
-echo "cd ../carpedm20.github.io"
+echo "cp -r ./output/* ../carpedm20.github.com/"
+echo "cd ../carpedm20.github.com"
 echo "cp ./about/index.html ."
 echo "git add ."
 echo "git commit -m 'blog update'"
