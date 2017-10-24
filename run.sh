4f93b5315eb9f5ec4be7df0b52b2271be0621cf4
nophd <carpedm20@gmail.com>
2017-10-24 22:53:31 +0900

    update

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@
 #cp -r ../old_carpedm20.github.com/randomwall ./output/
 pelican content; git add . --all; git commit -m "update"; git push origin master;
 echo "cp -r ./output/* ../carpedm20.github.io/"
-echo "cd ../carpedm20.github.com"
+echo "cd ../carpedm20.github.io"
 echo "cp ./about/index.html ."
 echo "git add ."
 echo "git commit -m \"update\""
